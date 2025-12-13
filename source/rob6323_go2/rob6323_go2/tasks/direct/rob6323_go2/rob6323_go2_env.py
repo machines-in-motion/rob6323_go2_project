95821a1b41b12c0a8b135af2b4748cbae777ef9e
Nishant-ZFYII <nishantpushparaju@gmail.com>
2025-12-13 11:40:35 -0500

    Part 5 updated logging for tensorboard

--- source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env.py
+++ source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env.py
@@ -47,6 +47,10 @@ class Rob6323Go2Env(DirectRLEnv):
                 "track_ang_vel_z_exp",
                 "rew_action_rate",     # <--- Added
                 "raibert_heuristic"    # <--- Added
+                "orient", #Part5 added. 
+                "lin_vel_z",
+                "dof_vel",
+                "ang_vel_xy",
                 
             ]
         }
@@ -139,6 +143,10 @@ class Rob6323Go2Env(DirectRLEnv):
             "track_lin_vel_xy_exp": lin_vel_error_mapped * self.cfg.lin_vel_reward_scale * self.step_dt,
             "track_ang_vel_z_exp": yaw_rate_error_mapped * self.cfg.yaw_rate_reward_scale * self.step_dt,
             "raibert_heuristic": rew_raibert_heuristic * self.cfg.raibert_heuristic_reward_scale,
+            "orient": rew_orient * self.cfg.orient_reward_scale,
+            "lin_vel_z": rew_lin_vel_z * self.cfg.lin_vel_z_reward_scale,
+            "dof_vel": rew_dof_vel * self.cfg.dof_vel_reward_scale,
+            "ang_vel_xy": rew_ang_vel_xy * self.cfg.ang_vel_xy_reward_scale,
         }
         reward = torch.sum(torch.stack(list(rewards.values())), dim=0)
         # action rate penalization
