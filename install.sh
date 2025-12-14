20f5e5a77c3b210408e80d56d780aaf10039193e
Haizhou Zhao <haizhou_zhao@outlook.com>
2025-12-14 18:38:04 -0500

    finish

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 mkdir logs
-ssh -o StrictHostKeyChecking=accept-new burst "cd ~/rob6323_go2_project && sbatch --job-name='rob6323_${USER}' --mail-user='${USER}@nyu.edu' install.slurm '$@'"
+sbatch --job-name='rob6323_haizhou' --mail-user='hz3862@nyu.edu' install.slurm
 cd /scratch/$USER
 git clone https://github.com/isaac-sim/IsaacLab.git
