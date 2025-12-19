7c1331832e7aa77d0e0ec95836a5f58839bbeafb
Jotheesh Kummathi <66893484+jotheesh1729@users.noreply.github.com>
2025-12-19 14:17:44 -0500

    Update entry points for Rob6323Go2 environment

--- source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/__init__.py
+++ source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/__init__.py
@@ -14,10 +14,10 @@ from . import agents
 
 gym.register(
     id="Template-Rob6323-Go2-Direct-v0",
-    entry_point=f"{__name__}.rob6323_go2_env:Rob6323Go2Env",
+    entry_point=f"{__name__}.rob6323_go2_env:Rob6323Go2RoughEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.rob6323_go2_env_cfg:Rob6323Go2EnvCfg",
+        "env_cfg_entry_point": f"{__name__}.rob6323_go2_env_cfg:Rob6323Go2RoughEnvCfg",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
     },
 )
