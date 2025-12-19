73c8468736151f62d181c2efc963da353ad680db
Nishant-ZFYII <nishantpushparaju@gmail.com>
2025-12-18 20:02:34 -0500

    made the terrain almost flat. Increased the number of rows and cols.

--- source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
+++ source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
@@ -79,18 +79,18 @@ class Rob6323Go2EnvCfg(DirectRLEnvCfg):
         terrain_generator=TerrainGeneratorCfg(
             size=(8.0, 8.0),
             border_width=5.0,  # Flat spawn area
-            num_rows=10,
-            num_cols=10,
+            num_rows=64,
+            num_cols=64,
             horizontal_scale=0.1,
-            vertical_scale=0.005,
+            vertical_scale=0.002,
             slope_threshold=0.75,
             use_cache=False,
             curriculum=False,  
             sub_terrains={
                 "random_rough": terrain_gen.HfRandomUniformTerrainCfg(
                     proportion=1.0,  
-                    noise_range=(0.02, 0.10),
-                    noise_step=0.02,
+                    noise_range=(0.00, 0.01),
+                    noise_step=0.005,
                     border_width=0.25,
                 ),
             },
